e24344503c73c710c88520920ed6bde1443b3f27
Christian Mueller <smoki99@googlemail.com>
2018-01-08 22:34:52 +0100

    corrected bash

--- run.sh
+++ run.sh
@@ -4,18 +4,18 @@ echo "Starting Up Certification Imange"
 
 # Check environment variable for nginx server is set
 
-if [ -z "$NGINX"]; then
+if [ -z "${NGINX}"]; then
     echo "Error: Please set the variable NGINX, which defines the nginx server"
     exit 1
 fi
 
-if [ -z "$EMAIL"]; then
+if [ -z "${EMAIL}"]; then
     echo "Error: Please set an emailaddress to used"
     exit 1
 fi
 
-if [ ! "$(docker ps -q -f name=$NGINX)" ]; then
-    echo "Error: the defined docker container $NGINX is not running"
+if [ ! "$(docker ps -q -f name=${NGINX})" ]; then
+    echo "Error: the defined docker container ${NGINX} is not running"
     exit 1
 fi
 
@@ -29,4 +29,4 @@ for domain in "${ADDR[@]}"; do
  done
 
 # Start cron
-/usr/sbin/crond -f -s /crontab.txt
+/usr/sbin/crond -f
